defb3f9101b993df2c0d5d7d24f721e7f0961aae
Ben Williams <benwilli@microsoft.com>
2020-04-23 09:40:58 -0500

    update scripts to patch dispatch refresh command

--- .devcontainer/setup.ps1
+++ .devcontainer/setup.ps1
@@ -16,11 +16,13 @@ $deployCogModelScript = "$workspaceDir/va/VirtualAssistantSample/Deployment/Scri
 ReplaceInFile $deployCogModelScript 'dispatch create' 'dotnet /node_modules/botdispatch/bin/netcoreapp2.1/Dispatch.dll create'
 ReplaceInFile $deployCogModelScript 'dispatch init' 'dotnet /node_modules/botdispatch/bin/netcoreapp2.1/Dispatch.dll init'
 ReplaceInFile $deployCogModelScript 'dispatch add' 'dotnet /node_modules/botdispatch/bin/netcoreapp2.1/Dispatch.dll add'
+ReplaceInFile $deployCogModelScript 'dispatch refresh' 'dotnet /node_modules/botdispatch/bin/netcoreapp2.1/Dispatch.dll refresh'
 
 $updateCogModelScript = "$workspaceDir/va/VirtualAssistantSample/Deployment/Scripts/update_cognitive_models.ps1"
 ReplaceInFile $updateCogModelScript 'dispatch create' 'dotnet /node_modules/botdispatch/bin/netcoreapp2.1/Dispatch.dll create'
 ReplaceInFile $updateCogModelScript 'dispatch init' 'dotnet /node_modules/botdispatch/bin/netcoreapp2.1/Dispatch.dll init'
 ReplaceInFile $updateCogModelScript 'dispatch add' 'dotnet /node_modules/botdispatch/bin/netcoreapp2.1/Dispatch.dll add'
+ReplaceInFile $updateCogModelScript 'dispatch refresh' 'dotnet /node_modules/botdispatch/bin/netcoreapp2.1/Dispatch.dll refresh'
 
 $publishScript = "$workspaceDir/va/VirtualAssistantSample/Deployment/Scripts/publish.ps1"
 ReplaceInFile $publishScript 'bin\\Release\\netcoreapp3.0' 'bin\\release\\netcoreapp3.0'
